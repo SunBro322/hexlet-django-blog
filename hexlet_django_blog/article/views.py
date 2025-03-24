1a549f9943f01aac7421f8748a2c7152674afb3a
chesanov_ae <chesanov_ae@vsw.ru>
2025-03-24 15:33:44 +0300

    lesson 5

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('article')
+    #return HttpResponse('article')
+        return render(request, 'articles.html', context={
+        'name': 'Articles',
+    })
